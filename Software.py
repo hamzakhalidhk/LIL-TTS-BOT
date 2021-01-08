fbf77e29df3c43ad1940a69c76183d1fc0602845
Hamza Khalid <itshamzakhalidhk@gmail.com>
2021-01-09 01:19:41 +0500

    Code cleaned
    Comments added

--- Software.py
+++ Software.py
@@ -1,3 +1,4 @@
+#Importing necessary modules
 from gtts import gTTS
 import pygame
 from pygame import mixer
@@ -6,6 +7,8 @@ import serial
 import pymysql.cursors
 import os
 import time
+
+#Database connection
 connection = pymysql.connect(host='localhost',
                              user='root',
                              password='',
@@ -13,59 +16,71 @@ connection = pymysql.connect(host='localhost',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 ser = serial.Serial(port='COM12', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, timeout=10)
+
+#Initializing Pygame module
 pygame.init()
 
-display_width = 1200
-display_height = 600
+#Initializing UI
+displayWidth = 1200
+displayHeight = 600
 black = (0,0,0)
 white = (255,255,255)
+gameDisplay = pygame.display.set_mode((displayWidth,displayHeight))
+pygame.display.set_caption('Robo Hazel')
 
-gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('Robo Alex')
+#Setting clock and images' path
 clock = pygame.time.Clock()
-CurrentPath = os.path.dirname(__file__)
-eyeFolderPath = os.path.join(CurrentPath, 'images')
-m1 = pygame.image.load(os.path.join(eyeFolderPath, 'opened.png'))
-#m1 = pygame.image.load(os.path.join("images","opened.png"))
-m2 = pygame.image.load(os.path.join(eyeFolderPath,"closed.png"))
-eyeCurrentImage = 1
+currentPath = os.path.dirname(__file__)
+eyeFolderPath = os.path.join(currentPath, 'images')
+eyeOpened = pygame.image.load(os.path.join(eyeFolderPath, 'opened.png'))
+eyeClosed = pygame.image.load(os.path.join(eyeFolderPath,"closed.png"))
+isEyeClosed = true
 
-def gameloop(eyeCurrentImage):
+def hazelloop(eyeCurrentImage):
+  
     gameExit = False
-
     while not gameExit:
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                  gameExit = True
         gameDisplay.fill(white)
 
-        if (eyeCurrentImage == 1):
-            gameDisplay.blit(m1, (200, 200))
+        #Eye Blinking
+        if (isEyeClosed == true):
+            gameDisplay.blit(eyeOpened, (200, 200))
             time.sleep(0.5)
-        if (eyeCurrentImage == 2):
-            gameDisplay.blit(m2, (200, 200))
+        if (isEyeClosed == false):
+            gameDisplay.blit(eyeClosed, (200, 200))
             time.sleep(3)
-        if (eyeCurrentImage == 2):
-            eyeCurrentImage = 1
+        if (isEyeClosed == false):
+            isEyeClosed = true
         else:
-            eyeCurrentImage += 1
+            isEyeClosed = true
 
+        #Reading serial port's data    
         if (ser.isOpen()):
             string=ser.readline().decode('ascii')
             print(string)
+            
+            #If the data string exists
             if len(string)>0:
+              
+                #Parsing string
                 for i in range(0, len(string)):
                     if string[i] == ':':
                         check = string[i - 1]
+                        
+                        #If the flag is 'e', fetch the text message from the database and convert it into speech
                         if check == 'e':
                             with connection.cursor() as cursor:
-                                    # Read a single record
+                                # Reading the last text message from the database 
                                 sql = "SELECT `message` FROM `robo` ORDER BY 'id' DESC LIMIT 1"
                                 cursor.execute(sql)
                                 message = str(cursor.fetchone())
                                 print(message)
                                 serial_message=message[13:len(message) - 2]
 
+                            #Converting the text message into speech and saving it in a file     
                             tts = gTTS(text=serial_message, lang='en-us')
                             mixer.init()
                             sf = TemporaryFile()
@@ -73,20 +88,17 @@ def gameloop(eyeCurrentImage):
                             sf.seek(0)
                             mixer.music.load(sf)
                             pygame.time.Clock().tick(10)
-                        if check == 'w':
-                            #serial_signal = string[i + 2:len(string)]
-                            #if (string[i + 2] == '1' and string[i + 4] == '1' and string[i + 6] == '1'):
-                                #print("Announce")
-                               #time.sleep(100)
+                          
+                        #If the flag is 'w', play the saved audio  
+                        elif check == 'w':
                             mixer.music.play()
                             while pygame.mixer.music.get_busy():
                                 print("Playing")
                                 pygame.time.Clock().tick(10)
-                        #else:
-                            #print("Journey")
+                        
         pygame.display.flip()
         clock.tick(120)
 
-gameloop(eyeCurrentImage)
+hazelloop(eyeCurrentImage)
 pygame.quit()
 quit()
